e5d8900c5c340f83d4822f7b5bc10cde815cc735
Vadim Piven <vadim.piven@milaboratories.com>
2024-09-14 00:46:36 +0400

    Functional

--- model/src/index.ts
+++ model/src/index.ts
@@ -2,33 +2,44 @@ import {
   BlockModel,
   type InferOutputsType,
   type InferHrefType,
-  PColumn,
-  TreeNodeAccessor,
-  isPColumn
+  isPColumn,
+  PColumnIdAndSpec
 } from '@milaboratory/sdk-ui';
 
 export type BlockArgs = {};
 
 export type UiState = {
   settingsOpened: boolean;
-  settings: unknown;
+  mainColumn?: PColumnIdAndSpec;
+  enrichmentColumns: PColumnIdAndSpec[];
 };
 
 export const model = BlockModel.create<BlockArgs, UiState>('Heavy')
   .initialArgs({})
-  .sections([
-    {
-      type: 'link',
-      href: '/',
-      label: 'View'
-    }
-  ])
-  .output('pFrame', (render) => {
-    const collection = render.resultPool.getDataFromResultPool();
+  .sections([{ type: 'link', href: '/', label: 'View' }])
+  .output('pFrame', (ctx) => {
+    const collection = ctx.resultPool.getDataFromResultPool();
     if (collection === undefined || !collection.isComplete) return undefined;
 
-    const pColumns = collection.entries.map(({ obj }) => obj).filter(isPColumn);
-    return render.createPFrame(pColumns as PColumn<TreeNodeAccessor>[]);
+    const pColumns = collection.entries
+      .map(({ obj }) => obj)
+      .filter(isPColumn)
+      .filter((column) => {
+        const resourceType = column.data.resourceType;
+        if (resourceType.version !== '1') return false;
+        if (
+          // resourceType.name === 'PColumnData/Json' ||
+          resourceType.name === 'PColumnData/JsonPartitioned' ||
+          resourceType.name === 'PColumnData/BinaryPartitioned' ||
+          resourceType.name === 'PColumnData/Partitioned/JsonPartitioned' ||
+          resourceType.name === 'PColumnData/Partitioned/BinaryPartitioned'
+        )
+          return true;
+        return false;
+      });
+
+    if (pColumns.length === 0) return undefined;
+    return ctx.createPFrame(pColumns);
   })
   .done();
 
